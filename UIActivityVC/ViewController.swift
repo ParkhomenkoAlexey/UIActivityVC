0a4bf97f309c086bf83ddb9f9aece611bcef3575
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-19 22:31:29 +0300

    Initial Commit

---
+++ UIActivityVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIActivityVC
+//
+//  Created by Алексей Пархоменко on 19/08/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
