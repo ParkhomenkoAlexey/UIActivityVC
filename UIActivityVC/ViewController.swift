d2b2bac5de18bf65deefa3295a5f919c2423d7bd
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-20 09:57:59 +0300

    Конечный проект

--- UIActivityVC/ViewController.swift
+++ UIActivityVC/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let arrayOfSomething = [#imageLiteral(resourceName: "IMG_3C926EB4F345-2"), #imageLiteral(resourceName: "IMG_3C926EB4F345-1")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .gray
     }
 
-
+    @IBAction func actionButtonTapped(_ sender: UIBarButtonItem) {
+        print("123")
+        
+        let shareController = UIActivityViewController(activityItems: arrayOfSomething, applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("It is done!")
+            }
+        }
+        
+        shareController.popoverPresentationController?.barButtonItem = sender
+        shareController.popoverPresentationController?.permittedArrowDirections = .any
+        
+        present(shareController, animated: true, completion: nil)
+    }
+    
 }
 
